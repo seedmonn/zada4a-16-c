96f0ae81b05aab5301d44288f39f692e00577a45
seedmonn <seedmp322@yandex.ru>
2019-12-26 14:41:32 +0300

    asdasd

--- main.c
+++ main.c
@@ -49,7 +49,7 @@ int main(int argc, char* argv[]) //параметр будет выведен с
 		return -1;
 	}
 	errno_t fp;
-	fp = fopen_s(&file, "C:\\rename.txt", "rt");
+	fp = fopen_s(&file, "text.txt", "rt");
 	/*элементы argv[1]...argv[argc - 1] содержат указатели на параметры командной строки.*/
 	while (!feof(file)) //печатаю
 	{
